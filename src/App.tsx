b0f4ad3d92396c2d79b115ba1a7600dc009b542c
`felipe-t-maciel` <`felipe_t_maciel@estudante.sesisenai.org.br`>
2025-09-28 02:13:59 -0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Categories from './components/Categories';
-import WhatsAppButton from './components/WhatsAppButton';
+// import React from "react";
+import Header from "./components/Header";
+// import Hero from "./components/Hero";
+import Categories from "./components/Categories";
+import WhatsAppButton from "./components/WhatsAppButton";
+import Pixel from "./components/Pixel"; // 👈 novo
 
 function App() {
   return (
     <div className="min-h-screen bg-black">
+      <Pixel /> {/* 👈 inicializa o Pixel */}
       <Header />
-      {/* <Hero /> */} 
+      {/* <Hero /> */}
       <Categories />
       <WhatsAppButton />
     </div>
---
+++ src/components/Pixel.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+
+const Pixel = () => {
+  useEffect(() => {
+    // Adiciona o script oficial do pixel
+    const script = document.createElement("script");
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1025854769558511');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
+  }, []);
+
+  return (
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style={{ display: "none" }}
+        src="https://www.facebook.com/tr?id=1025854769558511&ev=PageView&noscript=1"
+      />
+    </noscript>
+  );
+};
+
+export default Pixel;
